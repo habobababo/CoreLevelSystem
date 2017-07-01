dfb1898604267385c4b313aadaaaa8a1be247ca6
habobababo <shadowman11221@gmail.com>
2017-07-01 16:54:45 +0200

    .
    .

--- levelsystem/lua/autorun/config.lua
+++ levelsystem/lua/autorun/config.lua
@@ -1,14 +1,9 @@
 
 C_Level = C_Level or {}
-C_Level.Level = C_Level.Level or {}
-
 
 C_Level.MaxXP = 100
 C_Level.MaxLevel = 1000
-
 C_Level.SaveLevelTime = 30
-
-
 C_Level.XPOverTimeTime = 100 // in seconds
 C_Level.MinPlayers = 0
 C_Level.XPOverTimeXP = 5
@@ -17,7 +12,6 @@ C_Level.XPOverTimeXP = 5
 // DO NOT EDIT BELOW HERE //
 
 if SERVER then
-  print("loading sadsdsadasdsdasdasdasd")
   include("levels/sv_base.lua")
   AddCSLuaFile("levels/cl_hud.lua")
 end
@@ -33,4 +27,8 @@ end
 
 function meta:C_GetXP()
   return self:GetNWInt("C_XP")
+end
+
+function meta:C_XPLeft()
+  return (C_Level.MaxXP - self:C_GetXP())
 end
