c70513673294afb8530a79a70cd01205a031f371
habobababo <shadowman11221@gmail.com>
2018-05-31 21:00:35 +0200

    update ulx

--- levelsystem/lua/autorun/config.lua
+++ levelsystem/lua/autorun/config.lua
@@ -3,10 +3,7 @@ C_Level = C_Level or {}
 
 C_Level.MaxXP = 100
 C_Level.MaxLevel = 1000
-C_Level.SaveLevelTime = 30
-C_Level.XPOverTimeTime = 100 // in seconds
 C_Level.MinPlayers = 0
-C_Level.XPOverTimeXP = 5
 
 
 // DO NOT EDIT BELOW HERE //
--- levelsystem/lua/ulx/modules/sh/cc_level.lua
+++ levelsystem/lua/ulx/modules/sh/cc_level.lua
@@ -16,4 +16,23 @@ local level = ulx.command( "Core_Level", "ulx levelup", ulx.c_levelup, "!levelup
 level:addParam{ type = ULib.cmds.PlayersArg }
 level:addParam{ type = ULib.cmds.BoolArg, invisible = true }
 level:defaultAccess( ULib.ACCESS_ADMIN )
-level:help( "Give a player an entity" )
+level:help( "" )
+
+function ulx.c_resetlevel( calling_ply, target_plys, should_silent )
+
+	for k,v in pairs( target_plys ) do
+		v:C_ResetPlayer()
+	end
+
+	if should_silent then
+		ulx.fancyLogAdmin( calling_ply, true, "#A set #T level to zero", target_plys, entity )
+	else
+		ulx.fancyLogAdmin( calling_ply, "#A set #T level to zero", target_plys, entity )
+	end
+
+end
+local reset = ulx.command( "Core_Level", "ulx resetlevel", ulx.c_resetlevel, "!resetlevel" )
+reset:addParam{ type = ULib.cmds.PlayersArg }
+reset:addParam{ type = ULib.cmds.BoolArg, invisible = true }
+reset:defaultAccess( ULib.ACCESS_ADMIN )
+reset:help( "" )
